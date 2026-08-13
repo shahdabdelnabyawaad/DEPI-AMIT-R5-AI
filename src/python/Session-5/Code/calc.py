61390e61a0d034189c05410cbc335dcd36aff620
Shahd Abdelnaby <shahdabdelnaby92@gmail.com>
2026-08-13 07:01:51 +0300

    Update Files

---
+++ src/python/Session-5/Code/calc.py
@@ -0,0 +1,12 @@
+ class Calc:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def sum (self):
+        return self.x + self.y    
+    def sub (self):
+        return self.x - self.y
+    def mul(self):
+        return self.x * self.y
+    def div(self):
+        return self.x / self.y  
---
+++ src/python/Session-5/Code/main.py
@@ -0,0 +1,6 @@
+from calc import Calc
+if __name__=="__main__":
+   print("Hello")
+   c = Calc(10,20)
+   print(c.sum())
+   print(c.sub())
